b0200c1d1b55d1427e0ba79861c90f6554566227
Hanz <hctura.official@gmail.com>
2019-10-13 09:33:30 +0800

    transfer

--- congregationms/reports/urls.py
+++ congregationms/reports/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import MFSDetail, MFSList
+from .views import MFSCreate, MFSDetail, MFSList, MFSUpdate
 
 app_name = 'reports'
 urlpatterns = [
     path('monthly-field-service/', MFSList.as_view(), name='mfs-index'),
-    path('monthly-field-service/<str:pk>/', MFSDetail.as_view(), name='mfs-detail')
+    path('monthly-field-service/<str:pk>/', MFSDetail.as_view(), name='mfs-detail'),
+    path('monthly-field-service/new/', MFSCreate.as_view(), name='mfs-create'),
+    path('monthly-field-service/update/<str:pk>/', MFSUpdate.as_view(), name='mfs-update')
 ]
--- congregationms/reports/views.py
+++ congregationms/reports/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView
 
 from .models import MonthlyFieldService
 
@@ -25,4 +26,40 @@ class MFSList(ListView):
 
 class MFSDetail(DetailView):
     model = MonthlyFieldService
+    context_object_name = 'report'
+
+
+class MFSCreate(CreateView):
+    model = MonthlyFieldService
+    fields = [
+        'publisher',
+        'month_ending',
+        'placements',
+        'video_showing',
+        'hours',
+        'return_visits',
+        'bible_study',
+        'comments'
+    ]
+
+    def form_valid(self, form):
+        messages.success(
+            self.request,
+            'Successfully created new Monthly Field Service Report.'
+        )
+        return super().form_valid(form)
+
+
+class MFSUpdate(UpdateView):
+    model = MonthlyFieldService
+    fields = [
+        'publisher',
+        'month_ending',
+        'placements',
+        'video_showing',
+        'hours',
+        'return_visits',
+        'bible_study',
+        'comments'
+    ]
     context_object_name = 'report'
