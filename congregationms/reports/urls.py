228b83c35788df3d83f4770ad9119d051a1e9203
hanz <hctura.official@gmail.com>
2019-10-14 19:20:51 +0800

    add 'publisher' to mfs history url

--- congregationms/reports/urls.py
+++ congregationms/reports/urls.py
@@ -8,7 +8,7 @@ from .views import (
 app_name = 'reports'
 urlpatterns = [
     path('monthly-field-service/', MFSList.as_view(), name='mfs-index'),
-    path('monthly-field-service/history/<str:publisher>/', MFSHistoryList.as_view(), name='mfs-history'),
+    path('monthly-field-service/history/publisher/<str:publisher>/', MFSHistoryList.as_view(), name='mfs-history'),
     path('monthly-field-service/delete/<str:pk>/', MFSDelete.as_view(), name='mfs-delete'),
     path('monthly-field-service/new/', MFSCreate.as_view(), name='mfs-create'),
     path('monthly-field-service/<str:pk>/', MFSDetail.as_view(), name='mfs-detail'),
