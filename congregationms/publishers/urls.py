50447cc98169bb03d0446cabffeeea395f7b57ea
hanz <hctura.official@gmail.com>
2019-10-16 01:46:47 +0800

    remove delete group url

--- congregationms/publishers/urls.py
+++ congregationms/publishers/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     PublisherCreate, PublisherDelete, PublisherDetail,
-    PublisherList, PublisherUpdate, GroupCreate, GroupDelete, GroupDetail, GroupList, GroupUpdate
+    PublisherList, PublisherUpdate, GroupCreate, GroupDetail, GroupList, GroupUpdate
 )
 
 
@@ -13,7 +13,6 @@ urlpatterns = [
     path('groups/new/', GroupCreate.as_view(), name='group-create'),
     path('groups/detail/<str:pk>/', GroupDetail.as_view(), name='group-detail'),
     path('groups/edit/<str:pk>/', GroupUpdate.as_view(), name='group-update'),
-    path('groups/delete/<str:pk>/', GroupDelete.as_view(), name='group-delete'),
     path('edit/<str:pk>/', PublisherUpdate.as_view(), name='update'),
     path('detail/<str:pk>/', PublisherDetail.as_view(), name='detail'),
     path('new/', PublisherCreate.as_view(), name='create'),
