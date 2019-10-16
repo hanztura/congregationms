ea262fb7a7091506dd1598d1ff325368fd34756c
hanz <hctura.official@gmail.com>
2019-10-16 21:42:32 +0800

    add group on MFS save

---
+++ congregationms/reports/forms.py
@@ -0,0 +1,29 @@
+from django.forms import ModelForm
+
+from .models import MonthlyFieldService
+
+
+class MFSForm(ModelForm):
+
+
+    class Meta:
+        model = MonthlyFieldService
+        fields = [
+            'publisher',
+            'month_ending',
+            'placements',
+            'video_showing',
+            'hours',
+            'return_visits',
+            'bible_study',
+            'comments'
+        ]
+
+    def save(self, commit=True):
+        mfs = super().save(commit=False)
+
+        group = mfs.publisher.group
+        mfs.group = group
+        mfs.save()
+
+        return mfs
