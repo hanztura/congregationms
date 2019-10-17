c2b8571135d717b29481c329ded72970b7fbb4d5
hanz <hctura.official@gmail.com>
2019-10-17 23:37:59 +0800

    new slug field. new ordering meta

--- congregationms/publishers/models.py
+++ congregationms/publishers/models.py
@@ -9,13 +9,18 @@ from system.models import Congregation as Cong
 # Create your models here.
 class Publisher(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    slug = models.SlugField(blank=True, unique=False)
+    slug = models.SlugField(unique=False)
     last_name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, blank=True)
     date_of_birth = models.DateField(blank=True, null=True)
     date_of_baptism = models.DateField(blank=True, null=True)
     contact_numbers = models.CharField(max_length=200, blank=True)
+
+
+    class Meta:
+        ordering = 'last_name', 'first_name', 'middle_name'
+
 
     def __str__(self):
         name = self.name
