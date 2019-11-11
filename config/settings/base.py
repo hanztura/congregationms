39cf3f57745650f247c0ef0d3ff63f8f8d5e5ad4
hanz <hctura.official@gmail.com>
2019-11-11 21:50:47 +0800

    allowed hosts

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.setdefault('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.12']
+ALLOWED_HOSTS = []
 
 
 # Application definition
