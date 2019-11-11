f9eb788c3853d7ac82227a80ade89a7356685bbd
hanz <hctura.official@gmail.com>
2019-11-11 23:50:32 +0800

    disable query if home/dashboard

--- congregationms/system/middlewares.py
+++ congregationms/system/middlewares.py
@@ -8,18 +8,20 @@ def user_has_no_user_group_middleware(get_response):
     def middleware(request):
         user = request.user
         if not user.is_anonymous and not user.is_superuser:
-            try:
-                groups = user.publisher_groups.prefetch_related('group').all()
-                _groups = [g.group for g in groups]
-            except Exception as e:
-                groups = user.publisher_groups.empty()
-            request.authorized_groups = groups
-            members= Member.objects.filter(group__in=_groups, is_active=True).select_related('publisher')
-            request.authorized_publisher_pks = [m.publisher_id for m in members]
-            if not request.authorized_groups:
-                msg = 'You have no authorization to work on any groups. Please \
-                        inform your administrator.'
-                messages.warning(request, msg)
+            if request.path not in ('/', '/dashboard'):
+                print(request.path)
+                try:
+                    groups = user.publisher_groups.prefetch_related('group').all()
+                    _groups = [g.group for g in groups]
+                except Exception as e:
+                    groups = user.publisher_groups.empty()
+                request.authorized_groups = groups
+                members= Member.objects.filter(group__in=_groups, is_active=True).select_related('publisher')
+                request.authorized_publisher_pks = [m.publisher_id for m in members]
+                if not request.authorized_groups:
+                    msg = 'You have no authorization to work on any groups. Please \
+                            inform your administrator.'
+                    messages.warning(request, msg)
 
         response = get_response(request)
 
