cf0537c967a131b0abe1fda7e534aa370210e744
hanz <hctura.official@gmail.com>
2019-11-18 23:47:41 +0800

    if anonymous or superuser

--- congregationms/system/middlewares.py
+++ congregationms/system/middlewares.py
@@ -7,6 +7,7 @@ def user_has_no_user_group_middleware(get_response):
 
     def middleware(request):
         user = request.user
+        # anonymous and superusers has no groups allowed
         if not user.is_anonymous and not user.is_superuser:
             if request.path not in ('/', '/dashboard'):
                 print(request.path)
@@ -15,16 +16,21 @@ def user_has_no_user_group_middleware(get_response):
                     _groups = [g.group for g in groups]
                 except Exception as e:
                     groups = user.publisher_groups.empty()
-                request.authorized_groups = groups
+
                 members= Member.objects.filter(group__in=_groups, is_active=True).select_related('publisher')
-                request.authorized_publisher_pks = [m.publisher_id for m in members]
+                authorized_publisher_pks = [m.publisher_id for m in members]
                 if not request.authorized_groups:
                     msg = 'You have no authorization to work on any groups. Please \
                             inform your administrator.'
                     messages.warning(request, msg)
+        else:
+            groups = user.publisher_groups.empty()
+            authorized_publisher_pks = []
+
+        request.authorized_groups = groups
+        request.authorized_publisher_pks = authorized_publisher_pks
 
         response = get_response(request)
-
         return response
 
     return middleware
