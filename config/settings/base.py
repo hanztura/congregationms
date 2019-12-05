c6b43949a8c810d500ba93336395f91e0ab21625
hanz <hctura.official@gmail.com>
2019-12-05 19:22:23 +0800

    new cities app

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'reports',
     'mailing',
     'servants',
+    'cities',
 
     'simple_history',
 ]
---
+++ congregationms/cities/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class State(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
