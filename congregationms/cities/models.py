c88ada2e6189f1ba04713623d36640c013fcb8e0
hanz <hctura.official@gmail.com>
2019-12-05 20:58:30 +0800

    import data on cities

--- congregationms/cities/models.py
+++ congregationms/cities/models.py
@@ -12,6 +12,7 @@ class Country(models.Model):
 class State(models.Model):
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=10)
+    import_code = models.CharField(max_length=10, blank=True)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
     def __str__(self):
