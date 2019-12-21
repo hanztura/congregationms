7b19a7424a6ce249293926fde0378ad4757d6bca
hanz <hctura.official@gmail.com>
2019-12-22 01:33:51 +0800

    removed models to be edited on site

--- congregationms/pioneering/admin.py
+++ congregationms/pioneering/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from .models import Pioneer, PioneerDetail
 
 # Register your models here.
-admin.site.register(Pioneer)
-admin.site.register(PioneerDetail)
+# admin.site.register(Pioneer)
+# admin.site.register(PioneerDetail)
--- congregationms/servants/admin.py
+++ congregationms/servants/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 from .models import Servant
 
 
-admin.site.register(Servant)
+# admin.site.register(Servant)
